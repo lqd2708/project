ac3f718b8ea6e56ad4029988a3a76bb437cb6094
Dai Lam <lamquocdai2708@hotmail.com>
2018-11-21 16:21:17 +1000

    Updates

---
+++ precache-manifest.35fa7069ecd49b4801d6d034116b2d8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6be49d2b1a29501bf76",
+    "url": "/lqd2708/project.git/static/js/runtime~main.d6be49d2.js"
+  },
+  {
+    "revision": "83919b32028e014700f2",
+    "url": "/lqd2708/project.git/static/js/main.83919b32.chunk.js"
+  },
+  {
+    "revision": "5b966dc2e69f15274453",
+    "url": "/lqd2708/project.git/static/js/1.5b966dc2.chunk.js"
+  },
+  {
+    "revision": "83919b32028e014700f2",
+    "url": "/lqd2708/project.git/static/css/main.d06c35db.chunk.css"
+  },
+  {
+    "revision": "ffc9485998f8764206d1940fb4dd7a4c",
+    "url": "/lqd2708/project.git/index.html"
+  }
+];
